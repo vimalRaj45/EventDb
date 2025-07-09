3ec3e2b1cfe6eb528d449dded4476a758a3e8474
vimalRaj45 <vimalraj5207@gmail.com>
2025-07-09 21:44:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1368,14 +1368,14 @@ app.post('/api/auth/reset-password/:token', async (req, res) => {
 // 📥 CREATE Internship (only logged-in user can create)
 app.post('/api/internships', authenticateToken, authorizeRole('admin'), async (req, res) => {
   const userId = req.user.id;
-  const { company_name, position, duration, start_date, end_date, imgurl, desc } = req.body;
+  const { company_name, position, duration, start_date, end_date, imgurl, description } = req.body;
 
   try {
     const result = await db.query(
       `INSERT INTO internships 
-        (user_id, company_name, position, duration, start_date, end_date, imgurl, "desc")
+        (user_id, company_name, position, duration, start_date, end_date, imgurl, description)
        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
-      [userId, company_name, position, duration, start_date, end_date, imgurl, desc]
+      [userId, company_name, position, duration, start_date, end_date, imgurl, description]
     );
     res.json(result.rows[0]);
   } catch (err) {
@@ -1428,7 +1428,7 @@ app.put('/api/internships/:id', authenticateToken, authorizeRole('admin'), async
       end_date,
       imgurl,
       status,
-      desc
+      description
     } = req.body;
 
     const result = await db.query(
@@ -1440,9 +1440,9 @@ app.put('/api/internships/:id', authenticateToken, authorizeRole('admin'), async
         end_date = $5,
         imgurl = $6,
         status = $7,
-        "desc" = $8
+        description = $8
       WHERE id = $9 RETURNING *`,
-      [company_name, position, duration, start_date, end_date, imgurl, status, desc, id]
+      [company_name, position, duration, start_date, end_date, imgurl, status, description, id]
     );
 
     res.json(result.rows[0]);
