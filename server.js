cef9ff35a4dda3c789c2794ae4c346e77685cc25
vimalRaj45 <vimalraj5207@gmail.com>
2025-08-04 22:04:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
 
 // Middleware
 app.use(cors({
-  origin: '',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
